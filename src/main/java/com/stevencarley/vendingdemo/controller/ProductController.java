301b4186206b362b494b86ae4e7236ea879ed4af
Steve Carley <steven.p.carley@gmail.com>
2021-10-29 10:10:38 -0400

    Endpoint to get products

--- src/main/java/com/stevencarley/vendingdemo/controller/ProductController.java
+++ src/main/java/com/stevencarley/vendingdemo/controller/ProductController.java
@@ -1,14 +1,20 @@
 package com.stevencarley.vendingdemo.controller;
 
+import com.stevencarley.vendingdemo.model.Product;
 import com.stevencarley.vendingdemo.service.ProductsService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
-@Controller
+@RestController
+@CrossOrigin(originPatterns = "http://localhost:*")
 public class ProductController {
 
     private final ProductsService productsService;
@@ -21,5 +27,10 @@ public class ProductController {
     @MessageMapping("/purchase/{id}")
     public void purchaseProduct(@DestinationVariable String id) {
         productsService.purchaseProduct(id);
+    }
+
+    @GetMapping("/products")
+    public List<Product> getProducts() {
+        return productsService.getProducts();
     }
 }
---
+++ src/test/java/com/stevencarley/vendingdemo/controller/ProductControllerTest.java
@@ -0,0 +1,41 @@
+package com.stevencarley.vendingdemo.controller;
+
+import com.stevencarley.vendingdemo.model.Product;
+import com.stevencarley.vendingdemo.service.ProductsService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class ProductControllerTest {
+
+    @InjectMocks
+    ProductController productController;
+
+    @Mock
+    ProductsService productsService;
+
+    @Test
+    void purchaseProductWillPurchaseProduct() {
+        productController.purchaseProduct("1");
+        verify(productsService).purchaseProduct("1");
+    }
+
+    @Test
+    void getProductsWillGetProducts() {
+        var product = Product.builder().id("1").price(BigDecimal.ONE).description("test").build();
+        when(productsService.getProducts()).thenReturn(List.of(product));
+        List<Product> result = productController.getProducts();
+        assertEquals(List.of(product), result, "Expecting products to match");
+        verify(productsService).getProducts();
+    }
+}
